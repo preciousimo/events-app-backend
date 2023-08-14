839dc4842cedb2185e49f04ae0d181040d94e43e
preciousimo <preciousimoniakemu@gmail.com>
2023-08-14 15:46:38 +0100

    Create event model

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from events.models import Event
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'qr_code', 'date']
+
+admin.site.register(Event, EventAdmin)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import qrcode
+from io import BytesIO
+from django.core.files.base import ContentFile
+from PIL import Image
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    qr_code = models.ImageField(blank=True, upload_to='qrcodes/')
+    date = models.DateField()
+    
+    class Meta:
+        verbose_name_plural = "Events"
+    
+    def __str__(self):
+        return str(self.name)
+    
+    def save(self, *args, **kwargs):
+        if not self.qr_code:
+            qrcode_img = qrcode.make(self.name)
+            canvas = Image.new('RGB', (qrcode_img.pixel_size, qrcode_img.pixel_size), 'white')
+            canvas.paste(qrcode_img)
+            fname = f'qr_code-{self.name}.png'
+            buffer = BytesIO()
+            canvas.save(buffer, 'PNG')
+            self.qr_code.save(fname, ContentFile(buffer.getvalue()), save=False)
+        super().save(*args, **kwargs)
